7d53f3614d2ca4b90bab33bbeb737f49d9960ec4
revocatus-fasthub <revocatus.robert@fasthub.co.tz>
2017-07-19 23:35:14 +0300

    table layout

--- src/main/java/tz/co/fasthub/survey/service/AnswerService.java
+++ src/main/java/tz/co/fasthub/survey/service/AnswerService.java
@@ -3,6 +3,8 @@ package tz.co.fasthub.survey.service;
 import tz.co.fasthub.survey.domain.Answer;
 import tz.co.fasthub.survey.domain.Question;
 
+import java.util.List;
+
 /**
  * Created by root on 6/20/17.
  */
@@ -12,7 +14,6 @@ public interface AnswerService {
     Answer getAnswerById(Long id);
     Answer saveByQnsId(Answer ans, Question qsn);
     Iterable<Answer> getAnswerByQsnId(Long id);
-
 
     Iterable<Answer> listAllAnswers();
 }
