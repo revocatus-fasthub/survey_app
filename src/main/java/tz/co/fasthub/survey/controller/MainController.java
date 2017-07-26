4b5b0b79cc3b3dbe9baa3664bcd2b8efeb9f9f45
naamini-fasthub <naamini-fasthub@github.com>
2017-07-26 14:40:51 +0300

    security updated&modified

--- src/main/java/tz/co/fasthub/survey/controller/MainController.java
+++ src/main/java/tz/co/fasthub/survey/controller/MainController.java
@@ -2,6 +2,7 @@ package tz.co.fasthub.survey.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
  * Created by root on 7/10/17.
@@ -16,6 +17,11 @@ public class MainController {
     @RequestMapping(value = "/surveyQuestions")
     public String surveyQuestions(){
         return "surveyQuestions";
+    }
+
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
+    public String login() {
+        return "login";
     }
 
 }
